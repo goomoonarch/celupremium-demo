368c7355e346ef3c92ff0e8d796a5ba22909a081
Jhoss <jhosser@live.com>
2024-07-27 20:20:18 -0500

    change to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { BuyDescription } from "./components/BuyDescription.jsx";
@@ -8,7 +8,7 @@ import { IPhone } from "./components/IPhone.jsx";
 import Layout from "./Layout.jsx";
 import { BagProvider } from "./context/bag.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
