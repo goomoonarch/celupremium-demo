e7ad7c20249e8bd7a9e876ea029e250b1fc32872
Jhoss <jhosser@live.com>
2024-07-30 15:40:32 -0500

    delete the base url for the functionament of the img whit absolute routes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  //base: "/celupremium-demo/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+});
